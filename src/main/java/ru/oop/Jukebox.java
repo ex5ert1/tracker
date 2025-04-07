1d83e29d9ab5dc6b1e84dd90f7b9b82d051ba825
E.Laptev <E.Laptev@amtelsoft.ru>
2025-04-07 15:24:12 +0300

    7.4 Вызов метода с аргументами

---
+++ src/main/java/ru/oop/Jukebox.java
@@ -0,0 +1,19 @@
+package ru.oop;
+
+public class Jukebox {
+    public void music(int position) {
+        if (position == 1) {
+            System.out.println("Пусть бегут неуклюже");
+        } else if (position == 2) {
+            System.out.println("Пусть бегут неуклюже");
+        } else {
+            System.out.println("Песня не найдена");
+        }
+    }
+
+    public static void main(String[] args) {
+        Jukebox petya = new Jukebox();
+        int position = 1;
+        petya.music(position);
+    }
+}
