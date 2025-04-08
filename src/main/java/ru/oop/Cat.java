d45dd9cda1befe66cd68d82ff34bac1f2847371d
E.Laptev <E.Laptev@amtelsoft.ru>
2025-04-08 14:48:13 +0300

    7.6 Поля объекта

--- src/main/java/ru/oop/Cat.java
+++ src/main/java/ru/oop/Cat.java
@@ -2,15 +2,38 @@ package ru.oop;
 
 public class Cat {
 
-    public String sound() {
-        String voice = "may-may";
-        return voice;
+    private String food;
+    private String nickName;
+
+    public void show() {
+        System.out.println(this.food);
+        System.out.println(this.nickName);
+    }
+
+    public void giveNick(String nick) {
+        this.nickName = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.show();
+        Cat name = new Cat();
+        name.giveNick("nick");
+        name.show();
     }
 }
+
+
+
+
 
