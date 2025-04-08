c5e82dae8b4ee40d9f664bf68ea73e506b7d9cd8
E.Laptev <E.Laptev@amtelsoft.ru>
2025-04-08 14:06:08 +0300

    7.5 Вызов метода с возвращаемым типом

--- src/main/java/ru/oop/DummyDic.java
+++ src/main/java/ru/oop/DummyDic.java
@@ -3,12 +3,12 @@ package ru.oop;
 public class DummyDic {
 
     public String engToRus(String eng) {
-        return eng;
+        return "Неизвестное слово. " + eng;
     }
 
     public static void main(String[] args) {
         DummyDic name = new DummyDic();
         String result = name.engToRus("жава");
-        System.out.println("Неизвестное слово. " + result);
+        System.out.println(result);
     }
 }
