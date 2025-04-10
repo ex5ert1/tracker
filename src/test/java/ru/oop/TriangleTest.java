5ca3bc02cce353ade71dd814a226d4bf0eb2abcf
E.Laptev <E.Laptev@amtelsoft.ru>
2025-04-10 18:30:06 +0300

    7.14 Рефакторинг, площадь треугольника

--- src/test/java/ru/oop/TriangleTest.java
+++ src/test/java/ru/oop/TriangleTest.java
@@ -3,6 +3,7 @@ package ru.oop;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.offset;
+import static org.junit.Assert.assertEquals;
 
 public class TriangleTest {
     @Test
@@ -14,5 +15,16 @@ public class TriangleTest {
         double rsl = triangle.area();
         double expected = 8;
         assertThat(rsl).isCloseTo(expected, offset(0.001));
+    }
+
+    @Test
+    public void testNonExistentTriangle() {
+        Point a = new Point(0, 0);
+        Point b = new Point(1, 1);
+        Point c = new Point(2, 2); // Точки лежат на одной прямой
+        Triangle triangle = new Triangle(a, b, c);
+        double area = triangle.area();
+
+        assertEquals(-1, area, 0.0001);
     }
 }
