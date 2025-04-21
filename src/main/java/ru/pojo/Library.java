c8597b8d0170f22b73bb174b516490b402b56004
E.Laptev <E.Laptev@amtelsoft.ru>
2025-04-21 20:36:06 +0300

    9.3  Массивы и модели

--- src/main/java/ru/pojo/Library.java
+++ src/main/java/ru/pojo/Library.java
@@ -2,28 +2,28 @@ package ru.pojo;
 
 public class Library {
     public static void main(String[] args) {
-        Book book1 = new Book("Война миров", 300);
-        Book book2 = new Book("Clean code", 700);
-        Book book3 = new Book("20 тысяч лье под водой", 450);
-        Book book4 = new Book("Три мушкетера", 2001);
-        Book[] books = new Book[4];
-        books[0] = book1;
-        books[1] = book2;
-        books[2] = book3;
-        books[3] = book4;
-        for (int i = 0; i < books.length; i++) {
-            Book all = books[i];
-            System.out.println(all.getName() + " - " + all.getpageCount());
+        Book[] books = {
+                new Book("Война миров", 300),
+                new Book("Clean code", 700),
+                new Book("20 тысяч лье под водой", 450),
+                new Book("Три мушкетера", 2001)
+        };
 
-        }
-        book1 = books[0];
-        books[0] = books[3];
-        books[3] = book1;
-        for (int i = 0; i < books.length; i++) {
-            Book all = books[i];
-            System.out.println(all.getName() + " - " + all.getpageCount());
+        System.out.println("Изначальный список книг:");
+        for (Book book : books) {
+            System.out.println(book.getName() + " - " + book.getpageCount());
         }
 
+        System.out.println("Меняю местами список книг:");
+        Book change = books[0];
+        books[0] = books[books.length - 1];
+        books[books.length - 1] = change;
+
+        for (Book book : books) {
+            System.out.println(book.getName() + " - " + book.getpageCount());
+        }
+
+        System.out.println("Вывожу Clean code если есть:");
         for (Book book : books) {
             if ("Clean code".equals(book.getName())) {
                 System.out.println(book.getName() + " - " + book.getpageCount());
