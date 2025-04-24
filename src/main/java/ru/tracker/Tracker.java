ddda8d48364fec1020a8b44e3d90822b92d77972
Egor Laptev <egor.laptev91@yandex.ru>
2025-04-24 16:10:36 +0300

    9.7 Tracker - Хранилище

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -22,4 +22,33 @@ public class Tracker {
         }
         return rsl;
     }
+
+    public Item[] findAll() {
+        Item[] result = new Item[size];
+        int index = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                result[index++] = items[i];
+            }
+        }
+        return result;
+    }
+
+    public Item[] findByName(String key) {
+        int found = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getName().equals(key)) {
+                found++;
+            }
+        }
+
+        Item[] result = new Item[found];
+        int index = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getName().equals(key)) {
+                result[index++] = items[i];
+            }
+        }
+        return result;
+    }
 }
