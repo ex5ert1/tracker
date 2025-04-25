a1b28feab477cc213a2f672cfafb3252371a107a
Egor Laptev <egor.laptev91@yandex.ru>
2025-04-25 17:30:47 +0300

    9.8 Метод замены заявки

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -14,15 +14,11 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
+        int index = indexOf(id);
+        if (index != -1) {
+            return items[index];
         }
-        return rsl;
+        return null;
     }
 
     public Item[] findAll() {
@@ -38,5 +34,24 @@ public class Tracker {
             }
         }
         return Arrays.copyOf(temp, count);
+    }
+
+    private int indexOf(int id) {
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        if (index != -1) {
+            item.setId(id);
+            items[index] = item;
+            return true;
+        }
+        return false;
     }
 }
