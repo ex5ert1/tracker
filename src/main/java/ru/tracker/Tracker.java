3cce4832d5992efdf29f840237809558fb54924c
Egor Laptev <egor.laptev91@yandex.ru>
2025-04-26 09:57:40 +0300

    9.9 Метод удаления заявки

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -58,10 +58,7 @@ public class Tracker {
     public void delete(int id) {
         int index = indexOf(id);
         if (index != -1) {
-            int start = index + 1;
-            int distPos = index;
-            int length = size - index - 1;
-            System.arraycopy(items, start, items, distPos, length);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
         }
