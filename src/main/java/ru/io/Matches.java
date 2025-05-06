fd7245fb8dcaf1347ed459fcfb086c9ab69392aa
Egor Laptev <egor.laptev91@yandex.ru>
2025-05-06 16:49:14 +0300

    10.3 Чтение числа из консоли

--- src/main/java/ru/io/Matches.java
+++ src/main/java/ru/io/Matches.java
@@ -8,36 +8,33 @@ public class Matches {
         System.out.println("Игра 11.");
         boolean turn = true;
         int count = 11;
+
         while (count > 0) {
             String player = turn ? "Первый игрок" : "Второй игрок";
-            System.out.println(player + " введите число от 1 до 3:");
-            int matches = Integer.parseInt(input.nextLine());
-            boolean validInput = false;
-            while (!validInput) {
-                if (input.hasNextInt()) {
-                    matches = input.nextInt();
-                    if (matches >= 1 && matches <= 3 && matches <= count) {
-                        validInput = true;
-                    } else {
-                        System.out.println("Неверный ввод. Введите число от 1 до 3, не превышающее количество оставшихся спичек.");
-                    }
-                } else {
-                    System.out.println("Неверный ввод. Введите число.");
-                    input.nextLine();
-                }
+            System.out.println(player + ", введите число от 1 до 3:");
+
+            while (!input.hasNextInt()) {
+                System.out.println("Ошибка! Введите корректное число.");
+                input.nextLine();
+            }
+
+            int matches = input.nextInt();
+            input.nextLine();
+
+            if (matches < 1 || matches > 3 || matches > count) {
+                System.out.println("Ошибка! Введите число от 1 до 3, не превышающее количество оставшихся спичек (" + count + ")");
+                continue;
             }
 
             count -= matches;
             System.out.println("Осталось спичек: " + count);
-
             turn = !turn;
         }
+
         if (!turn) {
-            System.out.println("Выиграл первый игрок");
+            System.out.println("Выиграл первый игрок!");
         } else {
-            System.out.println("Выиграл второй игрок");
+            System.out.println("Выиграл второй игрок!");
         }
     }
 }
-
-
