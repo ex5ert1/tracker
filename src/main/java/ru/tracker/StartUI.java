fb1f2888617ef933c3b534322c4330bd22543596
E.Laptev <E.Laptev@amtelsoft.ru>
2025-05-11 13:13:56 +0300

    10.5 Добавление заявки

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -1,19 +1,42 @@
 package ru.tracker;
 
-import ru.tracker.Item;
-
-import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class StartUI {
+
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.print("Выбрать: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select == 0) {
+                System.out.println("=== Создание новой заявки ===");
+                System.out.print("Введите имя: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+                System.out.println("Добавленная заявка: " + item);
+            } else if (select == 6) {
+                run = false;
+            }
+        }
+    }
+
+    private void showMenu() {
+        String[] menu = {
+                "Добавить новую заявку", "Показать все заявки", "Изменить заявку",
+                "Удалить заявку", "Показать заявку по id", "Показать заявки по имени", "Завершить программу"
+        };
+        System.out.println("Меню:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
+    }
+
     public static void main(String[] args) {
-
-        Item name = new Item(10, "Егор");
-        System.out.println(name);
-        Item item = new Item("Не знаю что выводить но допустим так");
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String formattedDate = item.getCreated().format(formatter);
-
-        System.out.println("Создано: " + formattedDate);
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
     }
 }
