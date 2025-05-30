f7908d460c0420701c67d9d550657f96be3948f7
Egor Laptev <egor.laptev91@yandex.ru>
2025-05-30 16:52:33 +0300

    10.13 Input

--- src/main/java/ru/tracker/MockInput.java
+++ src/main/java/ru/tracker/MockInput.java
@@ -1,6 +1,6 @@
 package ru.tracker;
 
-public class MockInput implements Input{
+public class MockInput implements Input {
     @Override
     public String askStr(String question) {
         return null;
