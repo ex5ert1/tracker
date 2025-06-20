f574a90fa9c472c7ce3fb409415c68feefd0ad1d
Egor Laptev <egor.laptev91@yandex.ru>
2025-06-20 15:56:16 +0300

    10.24 Тесты для StartUI

---
+++ src/test/java/ru/tracker/StartUITest.java
@@ -0,0 +1,71 @@
+package ru.tracker;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StartUITest {
+    @Test
+    void whenCreateItem() {
+        Input input = new MockInput(
+                new String[] {"0", "Item name", "1"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(),
+                new ExitAction()
+        };
+        new StartUI().init(input, tracker, actions);
+        assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
+    }
+
+    @Test
+    void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("old item");
+        tracker.add(item);
+
+        Input input = new MockInput(
+                new String[] {
+                        "0", "old item", "1", "2", String.valueOf(item.getId()), "new item", "3"
+                }
+        );
+
+        UserAction[] actions = {
+                new CreateAction(),
+                new FindAllAction(),
+                new ReplaceAction(),
+                new ExitAction()
+        };
+
+        new StartUI().init(input, tracker, actions);
+
+        Item updatedItem = tracker.findById(item.getId());
+        assertThat(updatedItem.getName()).isEqualTo("new item");
+    }
+
+    @Test
+    void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("item to delete");
+        tracker.add(item);
+
+        Input input = new MockInput(
+                new String[] {
+                        "0", "item to delete", "1", "3", String.valueOf(item.getId()), "4"
+                }
+        );
+
+        UserAction[] actions = {
+                new CreateAction(),
+                new FindAllAction(),
+                new ReplaceAction(),
+                new DeleteAction(),
+                new ExitAction()
+        };
+
+        new StartUI().init(input, tracker, actions);
+
+        Item deletedItem = tracker.findById(item.getId());
+        assertThat(deletedItem).isNull();
+    }
+}
