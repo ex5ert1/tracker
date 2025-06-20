ac64968f286afe7d6070b676f1cf243ebcdce9a1
Egor Laptev <egor.laptev91@yandex.ru>
2025-06-20 17:43:36 +0300

    10.24 Тесты для StartUI

--- src/test/java/ru/tracker/StartUITest.java
+++ src/test/java/ru/tracker/StartUITest.java
@@ -21,35 +21,45 @@ class StartUITest {
     @Test
     void whenReplaceItem() {
         Tracker tracker = new Tracker();
-        Item item = new Item("old item");
-        tracker.add(item);
+        Item item1 = new Item("first item");
+        tracker.add(item1);
+        Item item2 = new Item("second item");
+        tracker.add(item2);
 
         Input input = new MockInput(
-                new String[] { "0", String.valueOf(item.getId()), "new item", "1" }
+                new String[] { "0", String.valueOf(item2.getId()), "new item", "1" }
         );
         UserAction[] actions = {
                 new ReplaceAction(),
                 new ExitAction()
         };
         new StartUI().init(input, tracker, actions);
-        Item updatedItem = tracker.findById(item.getId());
+        Item updatedItem = tracker.findById(item2.getId());
         assertThat(updatedItem.getName()).isEqualTo("new item");
+        Item firstItem = tracker.findById(item1.getId());
+        assertThat(firstItem).isNotNull();
+        assertThat(firstItem.getName()).isEqualTo("first item");
     }
 
     @Test
     void whenDeleteItem() {
         Tracker tracker = new Tracker();
-        Item item = new Item("item to delete");
-        tracker.add(item);
+        Item item1 = new Item("first item");
+        tracker.add(item1);
+        Item item2 = new Item("second item");
+        tracker.add(item2);
         Input input = new MockInput(
-                new String[] { "0", String.valueOf(item.getId()), "1" }
+                new String[] { "0", String.valueOf(item1.getId()), "1" }
         );
         UserAction[] actions = {
                 new DeleteAction(),
                 new ExitAction()
         };
         new StartUI().init(input, tracker, actions);
-        Item deletedItem = tracker.findById(item.getId());
+        Item deletedItem = tracker.findById(item1.getId());
         assertThat(deletedItem).isNull();
+        Item secondItem = tracker.findById(item2.getId());
+        assertThat(secondItem).isNotNull();
+        assertThat(secondItem.getName()).isEqualTo("second item");
     }
 }
