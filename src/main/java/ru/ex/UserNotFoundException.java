16d3a037c15818d4e5c11c289f0d4f644b967ebe
Egor Laptev <egor.laptev91@yandex.ru>
2025-07-03 11:39:03 +0300

    11.6 Иерархия исключений

--- src/main/java/ru/ex/UserNotFoundException.java
+++ src/main/java/ru/ex/UserNotFoundException.java
@@ -1,6 +1,6 @@
 package ru.ex;
 
-public class UserNotFoundException extends Exception{
+public class UserNotFoundException extends Exception {
     public UserNotFoundException(String message) {
         super(message);
     }
