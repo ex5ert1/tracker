c0e525cb4c592da899bc2cde583baf7ee89aea71
Egor Laptev <egor.laptev91@yandex.ru>
2025-07-03 11:38:05 +0300

    11.6 Иерархия исключений

---
+++ src/main/java/ru/ex/UserStore.java
@@ -0,0 +1,41 @@
+package ru.ex;
+
+public class UserStore {
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        if (users == null) {
+            throw new UserNotFoundException("Массив пользователей не может быть null");
+        }
+        for (User user : users) {
+            if (user.getUsername().equals(login)) {
+                return user;
+            }
+            throw new UserNotFoundException("Пользователь с именем " + login + " не найден");
+        }
+        return null;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (user == null) {
+            throw new UserInvalidException("Пользователь не может быть null");
+        }
+        String username = user.getUsername();
+        if (!user.isValid()) {
+            throw new UserInvalidException("Пользователь невалиден");
+        }
+        if (username == null || username.length() < 3) {
+            throw new UserInvalidException("Имя пользователя должно содержать не менее 3 символов");
+        }
+        return true;
+    }
+
+        public static void main(String[] args) throws UserNotFoundException {
+            User[] users = {
+                    new User("Ivan Ivanov", true)
+            };
+            User user = findUser(users, "Ivan Ivanov");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        }
+    }
+
