8616e53defc72e8b66a58fac0ea0a067484b461e
Egor Laptev <egor.laptev91@yandex.ru>
2025-08-26 15:33:03 +0300

    Просто коммит с экзамена

---
+++ src/main/java/ru/max/Animal.java
@@ -0,0 +1,17 @@
+package ru.max;
+
+public class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public Animal() {
+
+    }
+
+    public  void method(int age) {
+        System.out.println(age);
+    }
+}
---
+++ src/main/java/ru/max/Dog.java
@@ -0,0 +1,8 @@
+package ru.max;
+
+public class Dog extends  Animal {
+
+    public void method(int age) {
+        System.out.println(7);
+    }
+}
