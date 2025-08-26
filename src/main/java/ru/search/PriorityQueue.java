235a2398821c44efc5deeba7b9ddb713e5c21e37
Egor Laptev <egor.laptev91@yandex.ru>
2025-08-26 11:36:56 +0300

    13.5 Очередь с приоритетом

--- src/main/java/ru/search/PriorityQueue.java
+++ src/main/java/ru/search/PriorityQueue.java
@@ -3,7 +3,7 @@ package ru.search;
 import java.util.LinkedList;
 
 public class PriorityQueue {
-    private LinkedList<Task> tasks = new LinkedList<>();
+    private final LinkedList<Task> tasks = new LinkedList<>();
 
     public void put(Task task) {
         int index = 0;
