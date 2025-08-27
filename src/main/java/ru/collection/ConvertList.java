505c2fb7a89c296feeb5e13c245ed801adc6ca59
Egor Laptev <egor.laptev91@yandex.ru>
2025-08-27 16:01:55 +0300

    13.8 Лист массивов в один лист Integer

---
+++ src/main/java/ru/collection/ConvertList.java
@@ -0,0 +1,17 @@
+package ru.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConvertList {
+    public static List<Integer> convert(List<int[]> list) {
+        List<Integer> result = new ArrayList<>();
+        for (int[] array : list) {
+            for (int number : array) {
+                result.add(number);
+            }
+        }
+        return result;
+    }
+}
+
