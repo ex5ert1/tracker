43cab1ae6af2945b2becfd41f32cbf5b77ce9c9e
Egor Laptev <egor.laptev91@yandex.ru>
2025-09-08 20:03:31 +0300

    14.4 Модели данных и HashSet

--- src/main/java/ru/collection/NotifyAccount.java
+++ src/main/java/ru/collection/NotifyAccount.java
@@ -2,9 +2,10 @@ package ru.collection;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class NotifyAccount {
-    public static HashSet<Account> sent(List<Account> accounts) {
+    public static Set<Account> sent(List<Account> accounts) {
         HashSet<Account> uniqueAccounts = new HashSet<>();
         uniqueAccounts.addAll(accounts);
         return uniqueAccounts;
