59de8a1f74bc9ea6c7633901b2c12b15f0faef76
Egor Laptev <egor.laptev91@yandex.ru>
2025-10-28 14:57:03 +0300

    20.1 Анонимные классы

---
+++ src/main/java/ru/collection/Calculator.java
@@ -0,0 +1,18 @@
+package ru.collection;
+
+public class Calculator {
+    public static int calc(int a, int b) {
+        return a + b;
+    }
+
+    public static int calc(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static int calc(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+
+    public static void main(String[] args) {
+    }
+}
