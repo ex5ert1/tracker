f258488751d40641c600d5d54498739916f232f8
Egor Laptev <egor.laptev91@yandex.ru>
2025-11-01 15:45:59 +0300

    20.3 Lambda блок

---
+++ src/main/java/ru/lambda/LambdaUsage.java
@@ -0,0 +1,24 @@
+package ru.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("eeeee", "a", "ccc", "dddd", "bb");
+
+        Comparator<String> comparator = (left, right) -> {
+            System.out.println(
+                    "Comparing '" + left + "' (length: " + left.length() +
+                            ") with '" + right + "' (length: " + right.length() + ")"
+            );
+            return Integer.compare(right.length(), left.length());
+        };
+
+        strings.sort(comparator);
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+}
