2024177b38cdc5f9b079c8f5c796980a1dad75f2
Egor Laptev <egor.laptev91@yandex.ru>
2026-02-03 12:08:11 +0300

    21.7 Уникальность элементов и сортировка

--- src/test/java/ru/stream/ProfilesTest.java
+++ src/test/java/ru/stream/ProfilesTest.java
@@ -1,10 +1,8 @@
 package ru.stream;
 
 import org.junit.jupiter.api.Test;
-
 import java.util.Arrays;
 import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ProfilesTest {
