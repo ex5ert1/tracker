95acd11aeb40f5bfe36b17bd3128588790202786
Egor Laptev <egor.laptev91@yandex.ru>
2026-02-03 12:06:19 +0300

    21.6 Список адресов

--- src/main/java/ru/stream/Profiles.java
+++ src/main/java/ru/stream/Profiles.java
@@ -10,4 +10,12 @@ public class Profiles {
                 .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
+
+    public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
+        return profiles.stream()
+                .map(Profile :: getAddress)
+                .sorted((a1, a2) -> a1.getCity().compareToIgnoreCase(a2.getCity()))
+                .distinct()
+                .collect(Collectors.toList());
+    }
 }
