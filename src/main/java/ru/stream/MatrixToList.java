51d61d618062212c42c2f1bfe1f3b7b94885882d
Egor Laptev <egor.laptev91@yandex.ru>
2026-02-05 13:47:08 +0300

    21.9 Преобразование матрицы чисел в список чисел

--- src/main/java/ru/stream/MatrixToList.java
+++ src/main/java/ru/stream/MatrixToList.java
@@ -3,7 +3,7 @@ package ru.stream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 public class MatrixToList {
     public static List<Integer> convert(Integer[][] matrix) {
