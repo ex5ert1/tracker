38e7f888f945e35d03fbae0c351e00bb41abdb9d
Egor Laptev <egor.laptev91@yandex.ru>
2026-02-05 12:15:35 +0300

    21.7 Уникальность элементов и сортировка

--- src/main/java/ru/stream/Profiles.java
+++ src/main/java/ru/stream/Profiles.java
@@ -10,4 +10,12 @@ public class Profiles {
                 .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
+
+    public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .sorted((a1, a2) -> a1.getCity().compareTo(a2.getCity()))  // сортируем по городу
+                .distinct()
+                .collect(Collectors.toList());
+    }
 }
