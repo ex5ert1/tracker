782264c8417f5ed5664d5d8b93572f711b2424d1
Egor Laptev <egor.laptev91@yandex.ru>
2026-03-13 19:46:31 +0300

    22.3 Local-variable type inference

--- src/main/java/ru/search/PhoneDictionary.java
+++ src/main/java/ru/search/PhoneDictionary.java
@@ -21,8 +21,8 @@ public class PhoneDictionary {
                 .or(byPhone)
                 .or(byAddress);
 
-        ArrayList<Person> result = new ArrayList<>();
-        for (Person person : persons) {
+        var result = new ArrayList<Person>();
+        for (var person : persons) {
             if (combine.test(person)) {
                 result.add(person);
             }
