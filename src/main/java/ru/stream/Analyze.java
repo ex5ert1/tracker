fa2f15014967c8b04f73d658e7e92b8485b75bd3
Egor Laptev <egor.laptev91@yandex.ru>
2026-03-24 18:16:52 +0300

    23.1 Аттестация

--- src/main/java/ru/stream/Analyze.java
+++ src/main/java/ru/stream/Analyze.java
@@ -40,16 +40,17 @@ public class Analyze {
                 .collect(Collectors.toList());
     }
 
-    public static Tuple bestStudent(Stream<Pupil> stream) {
+public static Tuple bestStudent(Stream<Pupil> stream) {
         return stream
-            .map(p -> new Tuple(
-                    p.name(),
-                    p.subjects()
+                .map(pupil -> {
+                    int totalScore = pupil.subjects()
                             .stream()
                             .mapToInt(Subject::score)
-                            .sum()))
-            .max(Comparator.comparingDouble(Tuple::score))
-            .orElse(null);
+                            .sum();
+                    return new Tuple(pupil.name(), totalScore);
+                })
+                .max(Comparator.comparingDouble(Tuple::score))
+                .orElse(null);
     }
 
     public static Tuple bestSubject(Stream<Pupil> stream) {
